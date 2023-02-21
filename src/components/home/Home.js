deee0481a2902cebfd63c6f9155fbafd52767921
Administrator <administrator@dmmuriithi.local>
2023-02-21 08:29:29 +0300

    adjusting layout

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,9 +9,9 @@ export default function Home(){
                     <img src={homeImage}
                      alt="home" className="max-w-sm rounded-lg" />
                     <div>
-                    <h1 className="text-8xl font-bold">Welcome to Mealy</h1>
+                    <h1 className="text-6xl font-bold">Welcome to Mealy</h1>
                     <h3 className="text-3xl font-bold p-4">You Order... We Deliver...</h3>
-                    <p className="py-6 text-teal-700 text-base">Your number one stop shop for meals and snacked delivered right at your doorstep for NO extra charge.</p>
+                    <p className="py-6 text-teal-700 text-base">Your number one stop shop for meals and snacks delivered right at your doorstep for NO extra charge.</p>
                     <Link to="/meals" className="btn btn-primary text-center bg-[#00A082] hover:bg-[#00A082] hover:text-white hover:font-semibold">Make Order</Link>
                     </div>
                 </div>
